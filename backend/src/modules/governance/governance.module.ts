7054bfbffb7575ac702e7083ccd5034461b47125
tech-adrian <a.emmanuelcaxton@gmail.com>
2026-03-25 00:41:24 +0100

    feat(governance): register entities in GovernanceModule
    - Add TypeOrmModule.forFeature() to register GovernanceProposal and Vote entities
    - Add GovernanceIndexerService to module providers
    - Import necessary TypeORM decorators and entities

--- backend/src/modules/governance/governance.module.ts
+++ backend/src/modules/governance/governance.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { GovernanceController } from './governance.controller';
 import { GovernanceService } from './governance.service';
+import { GovernanceIndexerService } from './governance-indexer.service';
 import { UserModule } from '../user/user.module';
+import { GovernanceProposal } from './entities/governance-proposal.entity';
+import { Vote } from './entities/vote.entity';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, TypeOrmModule.forFeature([GovernanceProposal, Vote])],
   controllers: [GovernanceController],
-  providers: [GovernanceService],
+  providers: [GovernanceService, GovernanceIndexerService],
 })
-export class GovernanceModule {}
+export class GovernanceModule { }
